fdfe4dbb0808abc176fb749e9f9af54617752c16
asifiqbal0607 <asif.iqbal0607@gmail.com>
2026-02-25 13:32:40 +0500

    fix css path

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import "./styles/global.css";
+// import './index.css'
 
 createRoot(document.getElementById('root')).render(<App />)
 
